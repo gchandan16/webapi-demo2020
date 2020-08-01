723c7795a90080f890779f8ec33001f956b5d34a
Chandan <gchandan16@gmail.com>
2020-08-01 12:23:39 +0530

    One Controller is Added

---
+++ Webapiproject/Webapiproject/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webapiproject.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult Index()
+        {
+            // this is the first Controller
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
